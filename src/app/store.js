8817c2dfe7e941693f8dad196751e90befc292c7
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2025-06-27 18:42:30 +0600

    Added constants.js, redux, thunk and slice for user, installed libraries

---
+++ src/app/store.js
@@ -0,0 +1,36 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import { userReducer } from '../features/user/userSlice.js';
+
+const usersPersistConfig = {
+  key: 'callCards:user',
+  storage,
+  whitelist: ['user'],
+};
+
+const rootReducer = combineReducers({
+  user: persistReducer(usersPersistConfig, userReducer),
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
+
+export const persistor = persistStore(store);
---
+++ src/features/user/userSlice.js
@@ -0,0 +1,135 @@
+import { createSlice } from '@reduxjs/toolkit';
+import {
+  checkInSeniorSpec,
+  deleteUser,
+  editEmployees,
+  getEmployee,
+  getEmployees,
+  login,
+  register,
+} from './userThunk';
+
+const initialState = {
+  user: null,
+  loginLoading: false,
+  loginError: null,
+  registerError: null,
+  registerLoading: false,
+};
+
+const UsersSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state) => {
+      state.loginLoading = true;
+      state.loginError = null;
+    });
+    builder.addCase(login.fulfilled, (state, { payload: res }) => {
+      state.user = {
+        id: res.id,
+        username: res.username,
+        name: res.full_name,
+        role: res.role,
+        sip: res.sip,
+        phone_number: res.phone_number,
+        token: res.token,
+      };
+      state.loginLoading = false;
+    });
+    builder.addCase(login.rejected, (state, { payload: error }) => {
+      state.loginError = error;
+      state.loginLoading = false;
+    });
+    builder.addCase(register.pending, (state) => {
+      state.registerLoading = true;
+    });
+    builder.addCase(register.fulfilled, (state) => {
+      state.registerLoading = false;
+    });
+    builder.addCase(register.rejected, (state, { payload: error }) => {
+      state.registerError = error;
+      state.registerLoading = false;
+    });
+    builder.addCase(getEmployee.pending, (state) => {
+      state.employeeLoading = true;
+    });
+    builder.addCase(getEmployee.fulfilled, (state, { payload: employee }) => {
+      state.employee = employee;
+      state.employeeLoading = false;
+    });
+    builder.addCase(getEmployee.rejected, (state) => {
+      state.employeeLoading = false;
+    });
+    builder.addCase(getEmployees.pending, (state) => {
+      state.employeesLoading = true;
+    });
+    builder.addCase(getEmployees.fulfilled, (state, { payload: employees }) => {
+      state.employees = employees;
+      state.employeesLoading = false;
+    });
+    builder.addCase(getEmployees.rejected, (state) => {
+      state.employeesLoading = false;
+    });
+    builder.addCase(editEmployees.pending, (state) => {
+      state.editEmployeeLoading = true;
+    });
+    builder.addCase(editEmployees.fulfilled, (state, { payload: res }) => {
+      state.editEmployee = res;
+      state.editEmployeeLoading = false;
+    });
+    builder.addCase(editEmployees.rejected, (state) => {
+      state.editEmployeeLoading = false;
+    });
+    builder.addCase(checkInSeniorSpec.pending, (state) => {
+      state.checkedSeniorLoading = true;
+    });
+    builder.addCase(checkInSeniorSpec.fulfilled, (state) => {
+      state.checkedSeniorLoading = false;
+    });
+    builder.addCase(checkInSeniorSpec.rejected, (state) => {
+      state.checkedSeniorLoading = false;
+    });
+    builder.addCase(deleteUser.pending, (state) => {
+      state.deleteLoading = true;
+    });
+    builder.addCase(deleteUser.fulfilled, (state) => {
+      state.deleteLoading = false;
+    });
+    builder.addCase(deleteUser.rejected, (state) => {
+      state.deleteLoading = false;
+    });
+  },
+  selectors: {
+    selectUser: (state) => state.user,
+    selectLoginLoading: (state) => state.loginLoading,
+    selectLoginError: (state) => state.loginError,
+    selectRegisterError: (state) => state.registerError,
+    selectRegisterLoading: (state) => state.registerLoading,
+    selectEmployees: (state) => state.employees,
+    selectEmployeesLoading: (state) => state.employeesLoading,
+    selectEmployee: (state) => state.employee,
+    selectEmployeeLoading: (state) => state.employeeLoading,
+    selectEditEmployee: (state) => state.editEmployee,
+    selectEditEmployeeLoading: (state) => state.editEmployeeLoading,
+    selectEmployeeForEdit: (state) => state.employeeForEdit,
+    selectEmployeeForEditLoading: (state) => state.employeeForEditLoading,
+    selectCheckedSeniorLoading: (state) => state.checkedSeniorLoading,
+    selectDeleteLoading: (state) => state.deleteLoading,
+  },
+});
+
+export const userReducer = UsersSlice.reducer;
+export const {
+  selectUser,
+  selectLoginLoading,
+  selectLoginError,
+  selectRegisterError,
+  selectRegisterLoading,
+} = UsersSlice.selectors;
+export const { logout } = UsersSlice.actions;
